f9f27c711cb2eb9e2b0515bde6faec65c6fee737
Kunyang Lu <luk@funkelab-ws1.int.janelia.org>
2021-06-29 16:47:43 -0400

    Add filtering by duplicate number

--- group_features.py
+++ group_features.py
@@ -2,55 +2,45 @@ import numpy as np
 import json
 import copy
 
-dataset = '20210609'
+dataset = '20210625'
 
-def get_skipped_synapses(features):
-
-    skipped_synapses = []
-    feature_names = ['cleft_mean_intensity', 'cleft_membrane_mean_intensity',
-    't-bars_mean_intensity', 'cytosol_mean_intensity',
-    'cleft_median_intensity', 'cleft_membrane_median_intensity',
-    't-bars_median_intensity','cytosol_median_intensity', 'post_count',
-    'num_vesicles', 'vesicle_sizes', 'vesicle_circularities']
-
-    for synapse in features:
-
-        for feature_name in list(synapse.keys())[5:]:
-
-            if feature_name.split('_')[-1] == 'intensity':
-                if synapse[feature_name] != None: break
-
-            elif feature_name in ['post_count', 'num_vesicles']:
-                if synapse[feature_name]!=0: break
-
-            elif feature_name.split('_')[0] == 'vesicle':
-                if len(synapse[feature_name])!=0: break
-                else:
-                    skipped_synapses.append(synapse)
-                    break
-    return skipped_synapses
-
-def filter_features_by_condition(features, condition):
+def filter_features_by_condition(features, condition, duplicate_number):
 
     feature_name = condition[1]
     feature_short_name = feature_name.split('_')[0]
-    skipped_synapses = get_skipped_synapses(features)
+
+    # prefilter features according to duplicate_number
+
+    if duplicate_number == 1 or duplicate_number > 2:
+
+        prefiltered_features = [synapse for synapse in features if
+                synapse['duplicate_number'] == duplicate_number]
+
+    elif duplicate_number == None:
+        prefiltered_features = features
+
+    else:
+        duplicate_synapse_ids = get_duplicate_synapse_ids(features)
+        prefiltered_features  = [synapse for synapse in features if
+                synapse['synapse_id'] in duplicate_synapse_ids]
+
+    # filter again based on condition
 
     if feature_short_name == 'cleft' or feature_short_name == 't-bars':
 
         mean_or_median = feature_name.split('_')[-2]
-        features_filtered = [synapse for synapse in features if
+        features_filtered = [synapse for synapse in prefiltered_features if
                       isinstance(synapse[f'cytosol_{mean_or_median}_intensity'],float) &
                       isinstance(synapse[f'cleft_membrane_{mean_or_median}_intensity'],float) &
                       isinstance(synapse[f'{feature_short_name}_{mean_or_median}_intensity'],float) ]
 
-    elif feature_name in ['post_count', 'num_vesicles']:
-        features_filtered = [synapse for synapse in features if
-                      synapse not in skipped_synapses]
-
-    elif feature_name.split('_')[0] == 'vesicle':
-        features_filtered = [synapse for synapse in features if
+    elif feature_short_name == 'vesicle':
+        features_filtered = [synapse for synapse in prefiltered_features if
                 len(synapse[feature_name])!=0]
+
+    elif feature_name in ['num_vesicles', 'post_count']:
+        features_filtered = prefiltered_features
+
     else:
         print(f'ERROR: feature name {feature_name} does not exist')
         return None
@@ -84,6 +74,26 @@ def extract_feature(features, condition):
 
     return features_by_types
 
+def get_duplicate_synapse_ids(features):
+
+    duplicate_sets = {}
+    duplicate_synapse_ids = set({})
+
+    for i, synapse in enumerate(features):
+
+        synapse_id = synapse['synapse_id']
+        if synapse_id in duplicate_sets:
+            duplicate_sets[synapse_id].append(i)
+        else:
+            duplicate_sets[synapse_id] = [i]
+
+    for synapse_id in duplicate_sets.keys():
+
+        if len(duplicate_sets[synapse_id]) > 1:
+            duplicate_synapse_ids.add(synapse_id)
+
+    return duplicate_synapse_ids
+
 def catalog_features_by_condition(features, condition):
 
     if condition[0] == 'by_nt_types':
@@ -116,7 +126,7 @@ def catalog_features_by_condition(features, condition):
 
     return feature_types, features_by_types, feature_values
 
-def group_features_by_conditions():
+def group_features_by_conditions(duplicate_number):
     '''
     Group synapse features by different conditions.
 
@@ -174,14 +184,17 @@ def group_features_by_conditions():
     conditions = conditions_by_nt_types + conditions_by_annotators
     print(f'conditions are {conditions}')
 
+    # 'None' duplicate_number means to group only duplicate features
     for condition in conditions:
 
         print(f'Extracting feature under condition {condition}')
-        features_filtered = filter_features_by_condition(features, condition)
+        features_filtered = filter_features_by_condition(
+            features,
+            condition,
+            duplicate_number)
 
         extracted_feature = extract_feature(features_filtered, condition)
         grouped_features.update({str(condition) : extracted_feature})
 
     return grouped_features
-
 
